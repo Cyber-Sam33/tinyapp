b1659cd5122983248afb385669ba1d52ec55aa92
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-24 19:03:05 +0000

    Delete URLs added

--- express_server.js
+++ express_server.js
@@ -49,6 +49,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
